9f065dfd437c764485fe62c414b1c243a7002f94
Lucas <lucasalvesap10@outlook.com>
2024-06-25 21:46:25 -0300

    Tentativa e errro 2.0

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
-'use client'
-import './globals.css'
+"use client"
 export default function Home(){
     return(
         <main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  "./src/**/*.{js,jsx,mdx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
 
